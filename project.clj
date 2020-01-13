6ec2d48d9d53ec14e4a07c8194f7d9eeeaa10c42
Jon Pither <jon.pither@gmail.com>
2020-01-13 08:54:43 +0000

    Reinstating tests

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/crux-labs/crux-decorators"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [juxt/crux-core "19.12-1.6.1-alpha"]
+                 [juxt/crux-rocksdb "19.12-1.6.1-alpha"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}})
