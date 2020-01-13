15f9237c4cdb3d61b11368c1678a6130e7ab1981
Jon Pither <jon.pither@gmail.com>
2020-01-13 08:26:59 +0000

    Initial commit - moved from official Crux repo

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject juxt/crux-decorators "0.1.0-SNAPSHOT"
+  :description "Crux Decorators"
+  :url "https://github.com/crux-labs/crux-decorators"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}})
